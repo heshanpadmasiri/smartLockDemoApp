bc4cd90212001e5d6ad81024a17894e9072508f0
heshan <heshanpadmasiri@hotmail.com>
2018-03-15 17:38:24 +0530

    added message string to bluetooth page so we can log changes

--- src/pages/bluetooth/bluetooth.ts
+++ src/pages/bluetooth/bluetooth.ts
@@ -18,21 +18,32 @@ import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 export class BluetoothPage {
 
   discoverdDevices = new Array<String>();
+  message:String;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     private bluetoothSerial: BluetoothSerial) {
+      // Ask user to enable bluetooth
+      this.message = 'waiting';
+      this.bluetoothSerial.enable()
+        .then((result)=>{})
+        .catch((error)=>{});
   }
 
-  ionViewDidLoad() {
-    
+  ionViewDidLoad() {        
     this.bluetoothSerial.discoverUnpaired()
       .then((devices)=>{
         devices.forEach(device => {
-          this.discoverdDevices.push(device.name);
+          this.discoverdDevices.push(device.name+':'+device.address);
         });
-      })
+      });
+    this.bluetoothSerial.connectInsecure('00:21:13:00:3D:68')
+      .subscribe(
+        sucess=> this.message = 'sucess',
+        error => this.message = 'error',
+        () => this.message='completed'
+      );
   }
 }
 
